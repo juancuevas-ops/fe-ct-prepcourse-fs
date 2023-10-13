6576a6b1121cc890d9b0402207326887fb756970
unknown <j.cuevasvtr@gmail.com>
2023-10-13 17:40:58 -0300

    modificado el numero 4

--- M03 JavaScript Fundamentos/Ejercicio 04.js
+++ M03 JavaScript Fundamentos/Ejercicio 04.js
@@ -4,21 +4,42 @@
 // ⛔️ Recuerda que debes utilizar el objeto global "Math".
 
 function elevarAlCuadrado(num) {
-   // Retorna el valor de "num" elevado al cuadrado.
-   // Tu código:
+// Retorna el valor de "num" elevado al cuadrado.
+// Tu código:
+if (typeof num === 'number') {
+return Math.pow(num, 2);
+} else {
+// soy henry porfavor tomar en cuenta Manejar el caso en el que la entrada no es un número
+return "Por favor, proporciona un número válido."
+ }
 }
+var numero = 5;
+var resultado = elevarAlCuadrado(numero);
+console.log("El cuadrado de " + numero + " es: " + resultado);
 
-function elevarAlCubo(num) {
-   // Retorna el valor de "num" elevado al cubo.
-   // Tu código:
+
+
+
+
+
+
+function elevarAlCubo(_num) {
+// Retorna el valor de "num" elevado al cubo.
+// Tu código:
+var resultado = Math.pow(_num, 3);
+return resultado;
 }
+var numero = 2;
+var resultadoCubo = elevarAlCubo(numero);
+console.log("El cubo de " + numero + " es " + resultadoCubo);
 
-function elevar(num, exponent) {
+
+function elevar(_num, _exponent) {
    // Retorna el valor de "num" elevado al exponente "exponent".
    // Tu código:
 }
 
-function redondearNumero(num) {
+function redondearNumero(_num) {
    // Redondea "num" al entero más próximo y retórnalo.
    // Tu código:
 }
